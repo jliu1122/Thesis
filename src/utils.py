3aefdb16c154fa4686288e641ec11fcdd8c0614c
jliu1122 <jiahao.200101@example.com>
2025-06-09 16:17:31 +0000

    add functions

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,20 @@ class CocoDataSet:
   def __init__(self, captions, instance):
     self.instance = COCO(instance)
     self.captions = COCO(captions)
+
+  def loadImgsFromCategories(self, cats):
+    """
+    Retrieves the images object based on the category names
+
+    Args:
+      cats (str): List of category names
+    
+    Return:
+      list: A list of image objects.
+    """
+    catIDs = self.instance.getCatIds(catNms=cats)
+    imgIDs = self.instance.getImgIds(catIds=catIDs)
+
+    return self.instance.loadImgs(imgIDs)
+
+  
